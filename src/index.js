560be50722e9aff4afcadf916f6e13a137b19cfb
belirofon <nic.bel59@gmail.com>
2021-04-29 13:04:23 +0500

    finished test task

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/main.js';
+// import el3 from './images/el3.svg';
+// import entericon from './images/entericon.svg';
+// import iconbeer from './images/iconbeer.svg';
+// import icon from './images/icon.svg';
+// import label from './images/label.svg';
+// import makeUp from './images/makeUp.svg';
+import './scss/main.scss';
